ec2e4568a66823feee469854bb164b16a25889af
Yutika2309 <yutikarege2309@gmail.com>
2024-05-18 13:29:11 +0530

    WIP

--- chatapp/models.py
+++ chatapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class ClassDetails(models.Model):
+    GRADE = (
+                ('eleventh', '11th'),
+                ('twelfth', '12th')
+            )
+
+    SUBJECT = (
+                ('physics','Physics'),
+                ('math', 'Math'),
+                ('history', 'History'),
+                ('geography', 'Geography'),
+                ('english', 'English'),
+                ('computer science', 'Computer Science'),
+                ('chemistry', 'Chemistry'),
+                ('biology', 'Biology')
+            )
+
+class TopicDetails(models.Model):
+    pass
