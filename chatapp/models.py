4085baecc3156f90ba6807f22f63d9a583b84fc8
Yutika2309 <yutikarege2309@gmail.com>
2024-05-18 18:54:14 +0530

    config done

--- chatapp/models.py
+++ chatapp/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 class ClassDetails(models.Model):
+
+    """
+        @description: Model for basic class related information
+        @created by: Yutika Rege
+    """
+
     GRADE = (
                 ('eleventh', '11th'),
                 ('twelfth', '12th')
@@ -18,5 +24,22 @@ class ClassDetails(models.Model):
                 ('biology', 'Biology')
             )
 
-class TopicDetails(models.Model):
-    pass
+    teacher_name = models.CharField(max_length=120, null=False, blank=False)
+    grade = models.CharField(choices=GRADE, null=False, blank=False)
+    subject = models.CharField(choices=SUBJECT, null=False, blank=False)
+
+    def __str__(self):
+        return self.grade + "-" + self.subject
+
+
+# class Syllabus(models.Model):
+#     """
+#         @description: Model for topics for a subject for a certain grade
+#         @created by: Yutika Rege
+#     """
+     
+#     grade = models.ForeignKey(ClassDetails, on_delete=models.CASCADE, null=True, blank=True)
+#     subject = models.ForeignKey(ClassDetails, on_delete=models.CASCADE, null=True, blank=True)
+#     syllabus = model
+    
+#     def __str__(self):
